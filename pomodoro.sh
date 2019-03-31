97e8273dc49c1f65e14192e9a36e326f1bb44a4e
Niels Sorensen <niels.ws@gmail.com>
2019-03-30 21:28:20 -0400

    Split into functions

--- pomodoro.sh
+++ pomodoro.sh
@@ -12,9 +12,39 @@ declare -i SLEEP_TIME
 SLEEP_TIME="$*" #1200 seconds == 20 minutes
 DURATION=0.7 # Length of time a sound should play.
 
-while true; do
-    sleep "$SLEEP_TIME";
-    for i in "${SEQUENCE[@]}"; do
-        afplay -t $DURATION "$SFX_PATH"/"$i"
-    done
+function usage () {
+    echo "usage: $(basename "$0") integer  positive number, in seconds"
+    exit 1
+}
+
+function osx_check () {
+    if [[ "$(uname -s)" != 'Darwin' ]]
+    then
+        printf "\n==> For use with Mac OS X\n"
+        exit 1
+    fi
+}
+
+function check_input () {
+    if [[ "$SLEEP_TIME" -gt 0 ]] || [[ "$SLEEP_TIME" =~ ^- ]]
+    then
+        usage
+    fi
+}
+
+function play_sequence () {
+for i in "${SEQUENCE[@]}"; do
+    afplay -t $DURATION "$SFX_PATH"/"$i"
 done
+}
+
+function main () {
+    osx_check
+    check_input
+    while true; do
+        sleep "$SLEEP_TIME";
+        play_sequence
+    done
+}
+
+main

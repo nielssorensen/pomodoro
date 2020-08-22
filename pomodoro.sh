c67e137d63175d41fa2067fc70608e175041e00a
Niels Sorensen <niels.ws@gmail.com>
2020-08-22 10:35:55 -0400

    Create intro tone and lots of TODOs...

--- pomodoro.sh
+++ pomodoro.sh
@@ -7,6 +7,7 @@ set -e
 
 SFX_PATH=/System/Library/Sounds/ # Sound effect path for OSX.
 declare -a SEQUENCE
+INTRO_SEQUENCE=(Ping.aiff Ping.aiff Ping.aiff Glass.aiff)
 SEQUENCE=(Hero.aiff Blow.aiff Blow.aiff Blow.aiff Submarine.aiff)
 declare -i SLEEP_TIME
 SLEEP_TIME="$*" #1200 seconds == 20 minutes
@@ -25,6 +26,13 @@ function osx_check () {
     fi
 }
 
+function play_intro() {
+    for i in "${INTRO_SEQUENCE[@]}";
+    do
+        afplay -t "${DURATION}" "${SFX_PATH}/${i}"
+    done
+}
+
 function check_input () {
     if [[ "$SLEEP_TIME" -le 0 ]]
     then
@@ -33,14 +41,27 @@ function check_input () {
 }
 
 function play_sequence () {
-for i in "${SEQUENCE[@]}"; do
-    afplay -t $DURATION "$SFX_PATH"/"$i"
-done
+    for i in "${SEQUENCE[@]}"; do
+        afplay -t "${DURATION}" "${SFX_PATH}/${i}"
+    done
+}
+
+# TODO: create a counter. this should increment a variable every time the loop runs
+# upon exit, display the number of times the loop ran
+# TODO: IF CTRL-Z was hit, it should record the number of seconds that the script was paused
+# TODO: create a break period end tone
+# TODO: at end of the pomodoro period, ask what was achieved in the period
+# TODO: at the end of the break period, ask what was done to recharge
+# TODO: generate a report of the answers the user provided for historical purposes; output to text file
+
+loop_counter() {
+    :
 }
 
 function main () {
     osx_check
     check_input
+    play_intro
     while true; do
         sleep "$SLEEP_TIME";
         play_sequence
